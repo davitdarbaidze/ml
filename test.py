33239304a8662be3f35cdded1d80ec772133c2c1
davitdarbaidze <daviddarbaidze@gmail.com>
2023-11-28 11:53:10 +0100

    lesson 5

--- test.py
+++ test.py
@@ -1,11 +1,25 @@
 import numpy as np
-np.random.seed(0)
+import nnfs
+from nnfs.datasets import spiral_data
+
+nnfs.init()
 
 X = [[1, 2, 3, 2.5],
      [2.0 ,5.0 ,-1.0 ,2.0],
      [-1.5, 2.7, 3.3, -0.8]]
 
+X, y = spiral_data(100,3)
 
+inputs = [0, 2, -1, 3.3, -2.7, 1.1, 2.2, -100]
+
+output = []
+
+for i in inputs:
+    output.append(max(0,i))
+
+print(output)
+
+# lesson4
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
         self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
@@ -13,13 +27,19 @@ class Layer_Dense:
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
 
-layer1 = Layer_Dense(4, 5)
-layer2 = Layer_Dense(5, 2)
 
+
+
+class Activation_ReLU:
+    def forward(self,inputs):
+        self.output = np.maximum(0, inputs)
+# lesson4
+
+layer1 = Layer_Dense(2, 5)
+activation1 = Activation_ReLU()
 layer1.forward(X)
-# print(layer1.output)
-layer2.forward(layer1.output)
-print(layer2.output)
+activation1.forward(layer1.output)
+print(activation1.output)
 
 # inputs = [[1, 2, 3, 2.5],[2.0 ,5.0 ,-1.0 ,2.0],[-1.5, 2.7, 3.3, -0.8]]
 
